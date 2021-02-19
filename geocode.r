923ab9ecc884fe5d8d1d9dd8e06d8d7e203e2537
Kelly Pierce <kellyannepierce@gmail.com>
2021-02-18 19:36:44 -0600

    clean version

--- geocode.r
+++ geocode.r
@@ -69,60 +69,14 @@ if(!interactive()){
   write.csv(geocodes, savename, row.names=FALSE, quote=TRUE)
 }
 
-# apply the geocode_handler to successive chunks of the input data frame
-#geocoded <- NULL
-#for(i in increments){
-#  start <- i
-#  stop <- min(i + (chunk_size - 1), length(addr[,1]))
-#  submit <- addr[start:stop,]
-#  geocode_result <- geocode_handler(submit)
-#  if(typeof(geocode_result)=="list"){
-#    geocoded <- bind_rows(geocoded, geocode_result)
-#  }else{ #todo: retry these chunks instead of skipping
-#    my_alert <- paste('Warning encountered while processing chunk ', start, '-', stop, ':', geocode_result, sep='')
-#    message(my_alert)
-#  }
-  # todo: this will not save the last chunk
-#  if(counter %% save_chunks == 0){
-#    savename = file.path(outf, paste("geocoded_addr_", i, ".csv", sep=''))
-#    print(savename)
-#    write.csv(geocoded, savename)
-#    geocoded=NULL
-#  }
-  # pause between query chunks
-#  counter = counter + 1
-#  Sys.sleep(10)
-#}
-
-# NOT RUN
-
-#addr <- read.csv('/corral-secure/projects/Cooks-ProTX/working_dpfs_data/complete_vic_perp_addr.csv')
-#head(addr)
-
-#test_addr <- addr[1,]
-#test_gc1 <- geocode(test_addr, street=Address, city=City, postalcode=Zip.Code, method='osm')
-#test_gc2 <- geo(street=test_addr$Address, city=test_addr$City, postalcode=test_addr$Zip.Code, method='osm')
-
-#test_gc1_census <- geocode(test_addr, street=Address, city=City, postalcode=Zip.Code, method='census')
-
-#test_block <- addr[1:100,]
-#start_t <- Sys.time()
-#start <- 1
-#stop <- min(1 + (chunk_size - 1), length(addr[,1]))
-#submit <- addr[start:stop,]
-#test_block_census <- geocode(submit, street=Address, city=City, postalcode=Zip.Code, method='census',
-#                             full_results=TRUE, return_type='geographies')
-#stop_t <- Sys.time()
-#print(stop_t - start_t)
-
 ################################
 ## testing exception handling ##
 ################################
 
 # an incorrectly formatted address tibble
-#test_addr1 <- as_tibble(x=list('addr'='516 Drury Ln', 'city'='Austin', 'zip'=78737))
+#test_addr1 <- as_tibble(x=list('addr'='10100 Burnet Rd', 'city'='Austin', 'zip'=78758))
 # a correctly formatted address tibble
-#test_addr2 <- as_tibble(x=list('Address'='516 Drury Ln', 'City'='Austin', 'Zip.Code'=78737))
+#test_addr2 <- as_tibble(x=list('Address'='10100 Burnet Rd', 'City'='Austin', 'Zip.Code'=78758)
 
 # returns a character string with the error/warning/message
 #g1 <- geocode_handler(data_chunk=test_addr1)
