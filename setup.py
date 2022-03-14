af5d7e733376e9d34f25d0bfefefc4df2446afda
Kelly Pierce <kellyannepierce@gmail.com>
2022-03-14 12:42:06 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='geocodetools',
+    version='0.1.1',
+    description='utilities for geocoding',
+    url='https://github.com/kellypierce/geocode_tools',
+    author='Kelly Pierce',
+    packages=['geocodetools'],
+    zipsafe=False)
